95f659fe877ece49e0b20c145236daf5a90e8d10
rdJS <127029284+VanThanhHoang@users.noreply.github.com>
2024-09-23 10:35:08 +0700

    ad

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Cấu hình view engine là EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Cung cấp giao diện từ EJS
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Ensure the 'xml' directory exists
+const xmlDir = path.join(__dirname, "xml");
+if (!fs.existsSync(xmlDir)) {
+  fs.mkdirSync(xmlDir);
+}
+
+// WebSocket logic
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    switch (data.type) {
+      case "screenshot":
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: "dump" }));
+          }
+        });
+        break;
+
+      case "click":
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify(data));
+        });
+        break;
+      case "open":
+        // Handle 'click' and 'open' actions
+        // send to all clients
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify(data));
+        });
+        console.log(`Action ${data.type} with text: ${data.data.text}`);
+        break;
+
+      case "fill":
+        // Handle 'fill' action
+        console.log(
+          `Fill action with hintText: ${data.data.hintText} and text: ${data.data.text}`
+        );
+        wss.clients.forEach((client) => {
+          client.send(JSON.stringify(data));
+        })
+        break;
+
+      case "dump":
+        // Handle 'dump' action
+        console.log("Dump action requested");
+        break;
+
+      default:
+        const date = new Date().toISOString().replace(/:/g, "-");
+        const filename = `dumpscreen_${date}.xml`;
+        const filepath = path.join(xmlDir, filename);
+        fs.writeFile(filepath, data.data.text, (err) => {
+          if (err) {
+            console.error("Error saving XML:", err);
+            ws.send(`Error saving XML: ${err.message}`);
+          } else {
+            console.log(`XML saved to ${filepath}`);
+            ws.send(`XML saved as ${filename}`);
+          }
+        });
+        break;
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Server lắng nghe trên cổng 8080
+server.listen(8080, () => {
+  console.log("Server is listening on port 8080");
+});
