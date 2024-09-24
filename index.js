5ce3195ff07e98787ee5e5ea59c48cab9badfd94
rdJS <127029284+VanThanhHoang@users.noreply.github.com>
2024-09-24 21:11:09 +0700

    ??
    ooo

--- index.js
+++ index.js
@@ -33,13 +33,16 @@ if (!fs.existsSync(xmlDir)) {
 }
 // route post 
 app.post('/stopapp', (req, res) => {
+  const name = req.query
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify({
         type: "stopApp",
+        text:name
       }));
     }
   });
+  console.log(`Action stopApp with data:`, name);
   res.send('stop app');
 });
 // WebSocket logic
@@ -56,16 +59,6 @@ wss.on("connection", (ws, req) => {
     }
 
     switch (data.type) {
-      case "screenshot":
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: "dump" }));
-          }
-        });
-        break;
-
-      case "click":
-      case "open":
       case "stopApp":
         wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
@@ -74,42 +67,8 @@ wss.on("connection", (ws, req) => {
         });
         console.log(`Action ${data.type} with data:`, data.data);
         break;
-      case "fill":
-        // Broadcast these actions to all clients
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(data));
-          }
-        });
-        console.log(`Action ${data.type} with data:`, data.data);
-        break;
-
-      case "dump":
-        console.log("Dump action requested");
-        break;
-
-      default:
-        const date = new Date().toISOString().replace(/:/g, "-");
-        const filename = `dumpscreen_${date}.xml`;
-        const filepath = path.join(xmlDir, filename);
-        wss.clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: "show",text:data.data.text }));
-          }
-        });
-        fs.writeFile(filepath, data.data.text, (err) => {
-          if (err) {
-            console.error("Error saving XML:", err);
-            ws.send(JSON.stringify({ type: "error", message: `Error saving XML: ${err.message}` }));
-          } else {
-            console.log(`XML saved to ${filepath}`);
-            ws.send(JSON.stringify({ type: "success", message: `XML saved as ${filename}` }));
-          }
-        });
-        break;
     }
   });
-
   ws.on("close", () => {
     console.log("Client disconnected");
   });
